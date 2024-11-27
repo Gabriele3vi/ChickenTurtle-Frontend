7cda8137d9d520606458b297b4ef01c688008e38
Gabriele Trevisan <gabriele.3vi@gmail.com>
2024-11-27 11:26:47 +0100

    Improved info pages

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -18,8 +18,18 @@
         </p>
     </div>
     <div class="flex">
-        <img src="mission.jpeg" alt="Mission logo">
+        <!-- <img class="h-1/2" src="mission.jpeg" alt="Mission logo">-->
     </div>
+</div>
+<div>
+    <h1 class="font-bold text-3xl mt-4 mb-2">Value proposition</h1>
+    <p>Chicken-Turtle empowers businesses to unlock the full potential of their marketing campaigns through a dual approach to audience insights. Combining real-time feedback from Chicken-Comments with in-depth sentiment analysis from Turtle-Taste, our tool delivers a holistic view of audience reactions and trends. 
+    <br>With just a YouTube URL, brands can quickly assess campaign performance, understand broader audience sentiment, and refine their strategies to maximize engagement and drive meaningful connections.
+    </p>
+
+    <h1 class="font-bold text-3xl mt-4 mb-2">Differentiation</h1>
+    <p>Differentation "Unlike typical sentiment analysis tools, Chicken-Turtle provides a unique dual-layered insights system: Chicken-Comments offers rapid, real-time feedback on ad campaign performance, while Turtle-Taste dives deep into broader audience sentiment, including influencer and related content. 
+    <br> This combination ensures brands gain a 360-degree understanding of audience reactions, something few competitors can match.</p>
 </div>
 
 <div class="mission">
