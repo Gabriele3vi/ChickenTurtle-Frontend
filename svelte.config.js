15816bc611a1e2e89d8267b2dac0f486ff5f0502
Gabriele Trevisan <gabriele.3vi@gmail.com>
2024-11-27 10:03:15 +0100

    Updated adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,9 +10,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: vercel({
-			runtime: 'nodejs18' // Use 'nodejs20.x' for Node 20
-		  })
+		adapter: adapter()
 	}
 };
 
